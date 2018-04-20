7ff3788eab5425b64130ae589e8a752a6a304b25
RickSiep <34576186+RickSiep@users.noreply.github.com>
2018-04-20 13:52:05 +0200

    Update script.js

--- websitev2/public/script/script.js
+++ websitev2/public/script/script.js
@@ -2,38 +2,44 @@ function Dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
 }
 
 
-function Dropdown1() {
-    document.getElementById("myDropdown1").classList.toggle("show");
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll('.grid-item');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var captionText2 = document.getElementById("caption2");
+
+for(i = 0; i<img.length; i++){
+  let images = img[i];
+  img[i].onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+  }
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn1')) {
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        var dropdowns = document.getElementsByClassName("dropdown-content1");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
